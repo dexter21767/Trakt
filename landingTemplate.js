dbb8159cb295966bee18c25a661216508b9a8e73
dexter21767 <ahmidiyasser@egmail.com>
2022-09-06 01:43:14 +0100

    first commit

---
+++ landingTemplate.js
@@ -0,0 +1,351 @@
+const STYLESHEET = `
+* {
+   box-sizing: border-box;
+}
+
+body,
+html {
+   margin: 0;
+   padding: 0;
+   width: 100%;
+   height: 100%
+}
+
+html {
+	background: fixed;
+	background-size: auto 100%;
+	background-size: cover;
+	background-position: center center;
+	background-repeat: repeat-y;
+}
+
+body {
+   display: flex;
+   background-color: transparent;
+   font-family: 'Open Sans', Arial, sans-serif;
+   color: white;
+}
+
+h1 {
+   font-size: 4.5vh;
+   font-weight: 700;
+}
+
+h2 {
+   font-size: 2.2vh;
+   font-weight: normal;
+   font-style: italic;
+   opacity: 0.8;
+}
+
+h3 {
+   font-size: 2.2vh;
+}
+
+h1,
+h2,
+h3,
+p,
+label {
+   margin: 0;
+   text-shadow: 0 0 1vh rgba(0, 0, 0, 0.15);
+}
+
+p {
+   font-size: 1.75vh;
+}
+
+ul {
+   font-size: 1.75vh;
+   margin: 0;
+   margin-top: 1vh;
+   padding-left: 3vh;
+}
+
+a {
+   color: green
+}
+
+a.install-link {
+   text-decoration: none
+}
+
+button {
+   border: 0;
+   outline: 0;
+   color: white;
+   background: #8A5AAB;
+   padding: 1.2vh 3.5vh;
+   margin: auto;
+   text-align: center;
+   font-family: 'Open Sans', Arial, sans-serif;
+   font-size: 2.2vh;
+   font-weight: 600;
+   cursor: pointer;
+   display: block;
+   box-shadow: 0 0.5vh 1vh rgba(0, 0, 0, 0.2);
+   transition: box-shadow 0.1s ease-in-out;
+}
+
+button:hover {
+   box-shadow: none;
+}
+
+button:active {
+   box-shadow: 0 0 0 0.5vh white inset;
+}
+
+#addon {
+   width: 90vh;
+   margin: auto;
+   padding-left: 10%;
+   padding-right: 10%;
+   background: rgba(0, 0, 0, 0.60);
+}
+
+.logo {
+   height: 14vh;
+   width: 14vh;
+   margin: auto;
+   margin-bottom: 3vh;
+}
+
+.logo img {
+   width: 100%;
+}
+
+.name, .version {
+   display: inline-block;
+   vertical-align: top;
+}
+
+.name {
+   line-height: 5vh;
+}
+
+.version {
+   position: absolute;
+   line-height: 5vh;
+   margin-left: 1vh;
+   opacity: 0.8;
+}
+
+.contact {
+   left: 0;
+   bottom: 4vh;
+   width: 100%;
+   margin-top: 1vh;
+   text-align: center;
+}
+
+.contact a {
+   font-size: 1.4vh;
+   font-style: italic;
+}
+
+.separator {
+   margin-bottom: 4vh;
+}
+
+.label {
+  font-size: 2.2vh;
+  font-weight: 600;
+  padding: 0;
+  line-height: inherit;
+}
+
+.btn-group, .multiselect-container {
+  width: 100%;
+}
+
+.btn {
+  text-align: left;
+}
+
+.multiselect-container {
+  border: 0;
+  border-radius: 0;
+}
+
+.input, .btn {
+  height: 3.8vh;
+  width: 100%;
+  margin: auto;
+  margin-bottom: 10px;
+  padding: 6px 12px;
+  border: 0;
+  border-radius: 0;
+  outline: 0;
+  color: #333;
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0 0.5vh 1vh rgba(0, 0, 0, 0.2);
+}
+`;
+function landingTemplate(){
+
+
+
+
+const manifest = require("./manifest.json");
+	
+const stylizedTypes = manifest.types.map(t => t[0].toUpperCase() + t.slice(1) + (t !== 'series' ? 's' : ''));
+	  
+	  
+
+  return `
+   <!DOCTYPE html>
+   <html style="background-image: url(${manifest.background});">
+
+   <head>
+      <meta charset="utf-8">
+      <title>${manifest.name} - Stremio Addon</title>
+      <link rel="shortcut icon" href="${manifest.logo}" type="image/x-icon">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" >
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.min.js"></script>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css" rel="stylesheet"/>
+      <style>${STYLESHEET}</style>
+   </head>
+
+	<body>
+      <div id="addon">
+         <div class="logo">
+            <img src="${manifest.logo}">
+         </div>
+         <h1 class="name">${manifest.name}</h1>
+         <h2 class="version">${manifest.version || '0.0.0'}</h2>
+         <h2 class="description">${manifest.description || ''}</h2>
+
+         <div class="separator"></div>
+        
+		<h3 class="gives">This addon has more :</h3>
+         <ul>
+            ${stylizedTypes.map(t => `<li>${t}</li>`).join('')}
+         </ul>
+		 
+		 <div class="separator"></div>
+         <label class="label" for="Authorize">Authorize access to your trakt account:</label>
+		  <h3 class="gives">please start with this before doing any changes because all changes will be discarded when you press it.</h3><br>
+		 <a id="Authorize" class="install-link" href="https://trakt.tv/oauth/authorize?client_id=18bde7dcd858c86f9593addf9f66528f8c1443ec1bef9ecee501d1c5177ce281&redirect_uri=https%3A%2F%2F2ecbbd610840-trakt.baby-beamup.club%2F&response_type=code">
+            <button name="Authorize">Authorize</button>
+         </a>
+			<div class="separator"></div>
+		 <label class="label" for="trakt_defualt">trakt list:</label><br>
+		 <input type="checkbox" id="trakt_trending" value="trakt_trending" checked> trending</input><br>
+		 <input type="checkbox" id="trakt_popular" value="trakt_popular" checked> Popular</input><br>
+		  
+		  <div class="separator"></div>
+		 
+		  <label class="label" for="trakt_lists">trakt public lists ids:</label>
+		  <h3 class="gives">please separate ids with commas or spaces only (example: 35,52,14 or 35 52 14)</h3>
+		 <br>
+			<input type="text" id="trakt_lists">
+		 <div class="separator"></div>
+		 
+		 
+			<label class="label" for="trakt_user">trakt list:</label><br>
+		 <input type="checkbox" id="trakt_watchlist" value="trakt_watchlist"> Watchlist</input><br>
+		 <input type="checkbox" id="trakt_rec" value="trakt_rec"> Recommendations</input><br>
+		  
+		  
+		  
+         <div class="separator"></div>
+
+         <a id="installLink" class="install-link" href="#">
+            <button name="Install">INSTALL</button>
+         </a>
+         <div class="contact">
+           <p>Or paste into Stremio search bar after clicking install</p>
+        </div>
+        
+        <div class="separator"></div>
+		<h3 class="contact">
+		    To contact add-on creator:
+		    <a href="mailto:${manifest.contactEmail}">${manifest.contactEmail}</a>
+		</h3>
+		<div class="separator"></div>
+      </div>
+	  
+	  <script type="text/javascript">
+
+          $(document).ready(function() {
+			$('#trakt_trending').click(function() {
+				generateInstallLink()
+			});
+			$('#trakt_popular').click(function() {
+				generateInstallLink()
+			});
+			$('#trakt_watchlist').click(function() {
+				generateInstallLink()
+			});
+			$('#trakt_rec').click(function() {
+				generateInstallLink()
+			});
+			$('#trakt_lists').change(function() {
+				generateInstallLink()
+			});
+			generateInstallLink();
+          });
+
+
+
+          function generateInstallLink() {
+			var lists = [];
+			var data = [];
+			
+			if($('#trakt_trending').is(':checked')){
+				lists.push($('#trakt_trending').val());
+			}else{
+			lists = lists.filter(function(value, index, arr){ 
+				return value != $('#trakt_trending').val();
+			})	
+			}
+			if($('#trakt_popular').is(':checked')){
+				lists.push($('#trakt_popular').val());
+			}else{
+			lists = lists.filter(function(value, index, arr){ 
+				return value != $('#trakt_popular').val();
+			})	
+			}
+			if($('#trakt_watchlist').is(':checked')){
+				lists.push($('#trakt_watchlist').val());
+			}else{
+			lists = lists.filter(function(value, index, arr){ 
+				return value != $('#trakt_watchlist').val();
+			})	
+			}
+			if($('#trakt_rec').is(':checked')){
+				lists.push($('#trakt_rec').val());
+			}else{
+			lists = lists.filter(function(value, index, arr){ 
+				return value != $('#trakt_rec').val();
+			})	
+			}
+			
+			data['lists']=lists.join(',');
+			data['ids']=$('#trakt_lists').val().replaceAll(' ',',');
+			var query = window.location.search.substring(1);
+			if(query){
+			
+			data[query.split('=')[0]]=query.split('=')[1];
+			}
+			
+			configurationValue = Object.keys(data).map(key => key + '=' + data[key]).join('|');
+			console.log(configurationValue);
+			const configuration = configurationValue && configurationValue.length ? '/' + configurationValue : '';
+			const location = window.location.host + configuration + '/manifest.json'
+              navigator.clipboard.writeText('https://' + location);
+              installLink.href = 'stremio://' + location;
+			}
+			
+      </script>
+	  
+	</body>
+
+	</html>`
+}
+
+module.exports = landingTemplate;
---
+++ trakt.js
@@ -0,0 +1,198 @@
+const axios = require('axios').default;
+
+const cinemeta = require('./cinemeta');
+const host = "https://api.trakt.tv";
+const myurl = "http://127.0.0.1:7515/";
+client = axios.create({
+            headers: {
+				"Content-type": "application/json",
+				"trakt-api-key": "18bde7dcd858c86f9593addf9f66528f8c1443ec1bef9ecee501d1c5177ce281",
+				"trakt-api-version": 2
+            }
+        });
+		
+		
+async function request(url,header) {
+
+    //console.log(url,'url');
+    return await client
+    .get(url,header)
+    .then(res => {
+
+        // console.log(`statusCode: ${res.status}`);
+        return res;
+
+    })
+    .catch(error => {
+        console.error(error);
+        console.log('error');
+    });
+
+}
+
+async function popular(type){
+	if (type == "movie"){ 
+	var url = `${host}/movies/popular`;
+	} else if (type == "series"){
+	var url = `${host}/shows/popular`;
+	}
+	var items = (await request(url)).data;
+	var metas = [];
+	var i = 0;
+	var c = 0
+	while (i<50 && i<items.length){
+    //for(let item of items){
+		var item = items[i];
+	console.log(i);
+	console.log(item);
+	if(item.ids.imdb){
+		console.log()
+		var meta = await cinemeta(type,item.ids.imdb);
+	if(meta){metas[c] = meta;c++;}
+    //return (data);
+	
+	//}
+	}
+	i++;}
+	console.log(metas);
+	return metas;	
+}
+
+async function trending(type,trakt_type){
+	if (type == "movie"){ 
+	var url = `${host}/movies/trending`;
+	} else if (type == "series"){
+	var url = `${host}/shows/trending`;
+	}
+	var items = (await request(url)).data;
+	var metas = [];
+	var i = 0;
+	var c = 0
+	while (i<50 && i<items.length){
+    //for(let item of items){
+		var item = items[i];
+	console.log(i);
+	console.log(item);
+	if(item[trakt_type].ids.imdb){
+		console.log()
+		var meta = await cinemeta(type,item[trakt_type].ids.imdb);
+	if(meta){metas[c] = meta;c++;}
+    //return (data);
+	
+	//}
+	}
+	i++;}
+	console.log(metas);
+	return metas;	
+}
+
+async function watchlist(type,trakt_type,access_token){ //working
+
+var header = {
+            headers: {
+				"Authorization": `Bearer ${access_token}`
+        }};
+	
+	var url = `${host}/sync/watchlist/${trakt_type}s/`; 
+	console.log(url);
+	var items = (await request(url,header)).data;
+	return await getMeta(items,type,trakt_type);
+}
+
+async function list(list_ids){ //working
+	var data = []
+	console.log(list_ids);
+	for (let i=0; i<list_ids.length;i++){
+		if(list_ids[i].split(':').length > 1){
+			var user_id = list_ids[i].split(':')[0];
+			var list_id = list_ids[i].split(':')[1];
+			var url = `${host}/users/${user_id}/lists/${list_id}/`;
+		}else{
+			var url = `${host}/lists/${list_ids[i]}/`; 
+		}
+		console.log(url);
+		var res = await request(url);
+			data[i] = {
+				"name": res.data.name,
+				"id" : res.data.ids.trakt,
+				
+			}
+	}
+	console.log(data);
+	return(data);
+}
+
+async function list_catalog(type,trakt_type,id) { 
+	console.log(type,id);
+    var url = `${host}/lists/${id}/items/${trakt_type}`;
+    var items = (await request(url)).data;
+	return await getMeta(items,type,trakt_type);
+}
+
+async function getMeta(items,type,trakt_type){
+var metas = [];
+	var i = 0;
+	var c = 0
+	while (i<50 && i<items.length){
+    //for(let item of items){
+		var item = items[i];
+		if(item.type == trakt_type){
+	console.log(i);
+	console.log(item);
+	if(item[item.type].ids.imdb){
+		console.log()
+		var meta = await cinemeta(type,item[item.type].ids.imdb);
+	if(meta){metas[c] = meta;}
+    //return (data);
+	c++;
+	//}
+	}}
+	i++;}
+	console.log(metas);
+	return metas;	
+	
+}
+
+async function recomendations(type,trakt_type,access_token){  //working
+	
+	var header = {
+            headers: {
+				"Authorization": `Bearer ${access_token}`
+        }};
+	
+	var url = `${host}/sync/recommendations/${trakt_type}s/`; 
+	
+	var items = (await request(url,header)).data;
+	console.log(items);
+	//return await getMeta(items,type,trakt_type);
+	
+}
+
+async function getToken(code){ //working
+	var data = {
+  "code": code,
+  "client_id": "18bde7dcd858c86f9593addf9f66528f8c1443ec1bef9ecee501d1c5177ce281",
+  "client_secret": "d4214fec4f6f994b79f03430f19441120a44c67441c06cc1dd10dc92d7967b0c",
+  "redirect_uri": myurl,
+  "grant_type": "authorization_code"
+};
+	var url = 'https://api.trakt.tv/oauth/token'; 
+	return axios.post(url,data).then(res =>{
+	console.log(res.data.access_token);
+	return(res.data.access_token)
+	}).catch(error => {
+	console.log(error);	
+	})
+}
+
+const userdata = {
+  access_token: '2dd98df87f431cc5a117fdebd2f023e29dd5c0bb86a3ca08147473730a4b85b3',
+  token_type: 'Bearer',
+  expires_in: 7889237,
+  refresh_token: 'aadad7c14f3a1bbb0e9ed4f17f1a2a7609e7c981e98267498fa04cd5949288a8',
+  scope: 'public',
+  created_at: 1662301232
+}
+
+
+module.exports = {getToken,watchlist,recomendations,list,list_catalog,popular,trending,request};
