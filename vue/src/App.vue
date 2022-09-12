f68e28c40027ea4ddd8030bbca43bd06e6966e9d
dexter21767 <ahmidiyasser@egmail.com>
2022-09-12 11:41:23 +0100

    install button

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,5 @@
 <template>
     <div>
-
-
         <!-- Search modal -->
         <div id="searchModal" ref="searchModal" tabindex="-1" aria-hidden="true"
             class="hidden overflow-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
@@ -83,6 +81,38 @@
                 </div>
             </div>
         </div>
+
+
+        <!-- Main modal -->
+<div id="defaultModal"  ref='instl' tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
+    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Donate to the addon
+                </h3>
+                <button @click="state.install.hide();"  type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-6 space-y-6">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+just Donate                </p>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+Donate ffs                </p>
+            </div>
+            <!-- Modal footer -->
+            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                <button data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Donate</button>
+                <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Install Addon</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 
         <div class="bg-img relative min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover bg-center relative items-center"  :style="`background-image: url(${manifest.background});`">
@@ -364,12 +394,11 @@
                         </div> -->
 
                         <div class="mt-10 flex flex-col">
-                            <button type="button"
-                                class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mr-2">Donate</button>
-                            <button type="button"
+                            <button  @click="state.install.show();  generateInstallUrl();"  type="button"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Install
                                 Addon</button>
                         </div>
+
 
                         <div class="mt-5 flex flex-col">
                             <p class="text-center text-gray-400">This addon was created by<br />
@@ -424,6 +453,7 @@ const state = reactive({
     dropdownTrending: null,
     lists_popular: null,
     lists_trending: null,
+    install: null,
 });
 
 const searchModal = ref();
@@ -431,12 +461,13 @@ const Button_popular = ref();
 const Menu_popular = ref();
 const Button_trending = ref();
 const Menu_trending = ref();
-
+const instl = ref();
 onMounted(() => {
     getListsOflists()
     state.dropdownPopular = new Dropdown(Menu_popular.value, Button_popular.value);
     state.dropdownTrending = new Dropdown(Menu_trending.value, Button_trending.value);
     state.modal = new Modal(searchModal.value);
+    state.install = new Modal(instl.value);
 });
 
 
