9614a2105d10b208fd6c8b96fbc36d85840200dc
dexter21767 <ahmidiyasser@egmail.com>
2022-09-12 12:23:46 +0100

    kinda fixed
    Signed-off-by: dexter21767 <ahmidiyasser@egmail.com>

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,128 @@
 <template>
     <div>
+        <!-- Main modal -->
+        <!-- Search modal -->
+        <div id="searchModal" ref="searchModal" tabindex="-1" aria-hidden="true"
+            class="hidden overflow-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+            <div class="relative p-4 w-full max-w-2xl h-full md:max-h-screen overflow-hidden">
+                <!-- Modal content -->
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 overflow-y-auto h-full">
+                    <!-- Modal header -->
+                    <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                        <h3 class="w-full text-xl font-semibold text-gray-900 dark:text-white mr-4">
+                            <form @submit.prevent="getLists" class="w-full">
+                                <label for="searchModalInput"
+                                    class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search
+                                    Trakt lists</label>
+                                <div class="relative">
+                                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                            fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                        </svg>
+                                    </div>
+                                    <input v-model="state.searchQuery" type="search" id="searchModalInput"
+                                        class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Search Trakt lists" required>
+                                    <button type="submit"
+                                        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                                </div>
+                            </form>
+                        </h3>
+                        <button type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            @click="state.modal.toggle">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <!-- Modal body -->
+                    <div class="p-6 space-y-6">
+                        <div class="flex flex-col w-full gap-3 z-10">
+                            <div v-for="item in state.searchResults" :key="item.id"
+                                class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                                <a href="#">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {{item.name}} <small>by {{item.user}}</small>
+                                        <span
+                                            class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{{item.likes}}
+                                            likes</span>
+                                        <span
+                                            class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Items
+                                            count: {{item.item_count}}</span>
+                                    </h5>
+                                </a>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{item.description}}</p>
+                                <button @click="addList(item)"
+                                    class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Add list
+                                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor"
+                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        <div class="bg-img relative min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover bg-center relative items-center"  :style="`background-image: url(${manifest.background});`">
+
+        <div id="installModal" ref='installModal' tabindex="-1" aria-hidden="true"
+            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
+            <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                <!-- Modal content -->
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <!-- Modal header -->
+                    <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                            Donate to the addon
+                        </h3>
+                        <button @click="state.install.hide();" type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            data-modal-toggle="defaultModal">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-fill=""
+                                style="--darkreader-inline-fill:currentColor;">
+                                <path fill-rule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <!-- Modal body -->
+                    <div class="p-6 space-y-6">
+                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            just Donate </p>
+                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            Donate ffs </p>
+                    </div>
+                    <!-- Modal footer -->
+                    <div
+                        class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                        <button data-modal-toggle="defaultModal" type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Donate</button>
+                        <button data-modal-toggle="defaultModal" type="button"
+                            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Install
+                            Addon</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-img relative min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover bg-center relative items-center"
+            :style="`background-image: url(${manifest.background});`">
             <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
             <div class="max-w-md w-full space-y-8 p-10 bg-white shadow-lg rounded-xl z-10">
 
@@ -33,10 +154,11 @@
                             <span class="text-xs font-semibold text-gray-600 py-2">Add personal lists (requires Trakt
                                 login)</span>
                             <div class="mt-5 flex flex-col items-center">
-                                <a href="https://trakt.tv/oauth/authorize?client_id=18bde7dcd858c86f9593addf9f66528f8c1443ec1bef9ecee501d1c5177ce281&redirect_uri=https%3A%2F%2F2ecbbd610840-trakt.baby-beamup.club%2F&response_type=code">
-                                <button type="button"
-                                    class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Login
-                                    to Trakt.tv</button></a>
+                                <a
+                                    href="https://trakt.tv/oauth/authorize?client_id=18bde7dcd858c86f9593addf9f66528f8c1443ec1bef9ecee501d1c5177ce281&redirect_uri=https%3A%2F%2F2ecbbd610840-trakt.baby-beamup.club%2F&response_type=code">
+                                    <button type="button"
+                                        class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Login
+                                        to Trakt.tv</button></a>
                             </div>
                             <div class="mt-5">
                                 <label class="inline-flex relative items-center cursor-pointer">
@@ -44,7 +166,8 @@
                                     <div
                                         class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
                                     </div>
-                                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Your Recommendations</span>
+                                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Your
+                                        Recommendations</span>
                                 </label>
                             </div>
                             <div class="mt-5">
@@ -53,7 +176,8 @@
                                     <div
                                         class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
                                     </div>
-                                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Your Watchlist</span>
+                                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Your
+                                        Watchlist</span>
                                 </label>
                             </div>
                         </div>
@@ -95,10 +219,12 @@
                         <span class="text-xs font-semibold text-gray-600 py-2 mt-10">Add lists</span>
 
                         <div class="flex rounded-md shadow-sm mt-5 w-full" role="group">
-                            <button type="button" class="grow py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            <button type="button"
+                                class="grow py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                                 Browse popular lists
                             </button>
-                            <button type="button" class="grow py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            <button type="button"
+                                class="grow py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
                                 Browse trending lists
                             </button>
                         </div>
@@ -218,7 +344,7 @@
                         </div> -->
 
                         <div class="mt-10 flex flex-col">
-                            <button  @click="state.install.show();  generateInstallUrl();"  type="button"
+                            <button @click="state.install.show();  generateInstallUrl();" type="button"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Install
                                 Addon</button>
                         </div>
@@ -226,7 +352,8 @@
 
                         <div class="mt-5 flex flex-col">
                             <p class="text-center text-gray-400">This addon was created by<br />
-                                <a href="https://github.com/dexter21767" target="_blank" class="text-purple-700">dexter21767</a>
+                                <a href="https://github.com/dexter21767" target="_blank"
+                                    class="text-purple-700">dexter21767</a>
                                 and
                                 <a href="https://github.com/rleroi" target="_blank" class="text-purple-700">rab1t</a>
                                 .
@@ -308,15 +435,15 @@ async function getListsOflists() {
 
 function generateInstallUrl() {
     const lists = [];
-    for(let list in state.lists){
-    console.log(state.lists[list])
-    if(state.lists[list].id){
-        lists.push(state.lists[list].id)
-    }else if(state.lists[list].sort){
-        lists.push(`${state.lists[list].user}:${state.lists[list].slug}:${state.lists[list].sort[0]}:${state.lists[list].sort[1]}`)
-    }else{
-        lists.push(`${state.lists[list].user}:${state.lists[list].slug}`)
-    }
+    for (let list in state.lists) {
+        console.log(state.lists[list])
+        if (state.lists[list].id) {
+            lists.push(state.lists[list].id)
+        } else if (state.lists[list].sort) {
+            lists.push(`${state.lists[list].user}:${state.lists[list].slug}:${state.lists[list].sort[0]}:${state.lists[list].sort[1]}`)
+        } else {
+            lists.push(`${state.lists[list].user}:${state.lists[list].slug}`)
+        }
     }
     console.log(lists);
 }
@@ -333,10 +460,9 @@ function addListUrl() {
         sort = sort.split('?')[1].split('=')[1].split(',');
     }
 
-    let id = sort.length? username+":"+slug+":"+sort[0]+":"+sort[1]:username+":"+slug;
+    let id = sort.length ? username + ":" + slug + ":" + sort[0] + ":" + sort[1] : username + ":" + slug;
 
-    state.lists.push({user:username, name:slug, slort: sort});
-    generateInstallUrl();
+    state.lists.push({ user: username, name: slug, slort: sort });
 }
 
 async function searchLists() {
@@ -349,9 +475,8 @@ function addList(list) {
         name: list.name,
         slug: list.id,
         username: list.user,
-        id:list.id
+        id: list.id
     });
-    generateInstallUrl();
 }
 
 function removeList(list) {
@@ -361,7 +486,6 @@ function removeList(list) {
     }
 
     state.lists.splice(index, 1);
-    generateInstallUrl();
 }
 </script>
 
@@ -394,7 +518,7 @@ h1 {
     cursor: -webkit-grabbing;
 }
 
-.bg-img{
+.bg-img {
     background: fixed;
     background-size: cover;
     background-position: center center;
