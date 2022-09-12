1030cac58c6650ffc81d925ed7bf895b3fd05e1e
dexter21767 <ahmidiyasser@egmail.com>
2022-09-12 17:51:26 +0100

    fixed indentations :p

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -233,18 +233,21 @@
                     <!-- Modal body -->
                     <div class="p-6 space-y-6">
                         <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            Hello guys, seems like we have reached the end of our journey together, most things are working properly and most features are implemented. a huge thanks to Rab1t for his tremendous help.</p>
+                            Hello guys, seems like we have reached the end of our journey together, most things are
+                            working properly and most features are implemented. a huge thanks to Rab1t for his
+                            tremendous help.</p>
                         <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            I have decided to drop support for this addon. so now it's up to Rab1t to keep maintaining it or not. otherwise the source code is always available on my github.<br>Peace.</p>
+                            I have decided to drop support for this addon. so now it's up to Rab1t to keep maintaining
+                            it or not. otherwise the source code is always available on my github.<br>Peace.</p>
                     </div>
                     <!-- Modal footer -->
                     <div
                         class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
                         <a id="install_button" href="#"><button type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Install
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Install
                                 Addon</button></a>
-                       <button type="button" @click="state.install.hide();"
-                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancle</button>
+                        <button type="button" @click="state.install.hide();"
+                            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancle</button>
                     </div>
                 </div>
             </div>
