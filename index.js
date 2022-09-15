0097ea1967d3d5609302d55133a6d9e2c234ff25
dexter21767 <ahmidiyasser@egmail.com>
2022-09-15 06:07:58 +0100

    Auto-commit

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const sort = ["added asc", "added desc", "title asc", "title desc", "released as
 app.get('/', (req, res) => {
 	if (req.query.code) {
 		getToken(req.query.code).then(data => {
-			if (data== undefined){
+			console.log(data)
+			if (data !== undefined){
 			res.redirect('/configure/?access_token=' + data);
 		}else{	
 			res.redirect('/configure/?access_token_undefined');
