ac3fe74964e8f13281a713377e29fd844cb7828b
dexter21767 <ahmidiyasser@gmail.com>
2023-11-30 14:51:34 +0100

    Auto-commit

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: [
+        // other external modules
+        'flowbite/src/components/modal'
+      ]
+    }
+  }
 })
